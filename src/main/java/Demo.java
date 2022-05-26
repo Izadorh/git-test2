6ddfe9d0dcc482699f57d41441753588d82a0208
Jerry <1378036718@qq.com>
2022-05-26 20:45:57 +0800

    hot-fix commit1

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("第一次修改");
+        System.out.println("hot-fix修改");
     }
 }
